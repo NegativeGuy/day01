1e7e886e1ea3af9cc23d12a2b3a706e009aaf912
dydgns2446 <dydgns2446@naver.com>
2021-07-23 11:19:07 +0900

    주석 방법

--- src/day01/EX01.java
+++ src/day01/EX01.java
@@ -2,7 +2,14 @@ package day01;
 
 public class EX01 {
 	public static void main(String[] args) {
+		// sysout은 괄호 안의 내용을 출력하는 기능
+		// ;(세미콜론)은 문장의 끝을 알리는 용도
+		/* 문단 주석 입니다 */
+		/*
 		System.out.println("hello world");
 		System.out.println("this is java class");
+		*/
+		System.out.print("hello");
+		System.out.print("this is java class");
 	}
 }
