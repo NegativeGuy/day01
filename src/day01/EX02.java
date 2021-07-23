99e27c37de995e1ae0d94b5e34a29f6b5e0d0976
dydgns2446 <dydgns2446@naver.com>
2021-07-23 11:31:56 +0900

    EX02 생성

---
+++ src/day01/EX02.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class EX02 {
+	public static void main(String[] args) {
+		/* 이스케이프 문자
+		  \n : 키보드 enter 역할(개행)
+		  \t : tab 크기만큼 오른쪽으로 커서 이동(8칸 정도)
+		  \" : "(쌍따옴표)를 일반 문자로 사용하고자 할 때
+		  \\ " \(역슬래쉬)를 표현 할 때 사용
+		  
+		*/
+	}
+}
