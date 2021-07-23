55096c1c830669d1d8a3ca22f3cfe7a1cef09ae4
dydgns2446 <dydgns2446@naver.com>
2021-07-23 12:49:05 +0900

    문자열 관련 기능

--- src/day01/EX02.java
+++ src/day01/EX02.java
@@ -7,7 +7,11 @@ public class EX02 {
 		  \t : tab 크기만큼 오른쪽으로 커서 이동(8칸 정도)
 		  \" : "(쌍따옴표)를 일반 문자로 사용하고자 할 때
 		  \\ " \(역슬래쉬)를 표현 할 때 사용
-		  
-		*/
+	    */
+		
+		// " " : 쌍따옴표로 묶인 값은 문자열로 인식한다
+		System.out.println("He\nllo Java");
+		System.out.println("1\t1234567\t12345678\t12");
+		System.out.println("test \"123\" \\456");
 	}
 }
